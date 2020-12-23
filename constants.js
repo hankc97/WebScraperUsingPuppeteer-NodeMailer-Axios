f7d84b9d089561354f1ff1fca830bd00f8d77785
hankc97 <70183272+hankc97@users.noreply.github.com>
2020-12-22 20:17:17 -0500

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,21 @@
+//Discord Constants
+module.exports = {
+    XENITH_GENERAL_CHAT_DISCORD: 'DiscordLink',
+    XENITH_SWING_ALERTS_DISCORD: 'DiscordLink',
+
+    DISCORD_USERNAME: 'username',
+    DISCORD_PASSWORD: 'password',
+
+    EXPECTED_DISCORD_EMAIL_SELECTOR: '[name="email"]',
+    EXPECTED_DISCORD_PASSWORD_SELECTOR: '[name="password"]',
+    EXPECTED_DISCORD_SUBMITBUTTON_SELECTOR: '[type="submit"]',
+
+    DISCORD_MESSAGES_SELECTOR: ".messageContent-2qWWxC",
+
+    GMAIL_EMAIL_USERNAME: "username",
+    GMAIL_EMAIL_PASSWORD: "password",
+
+    SMS_Gateway: ['All Users@verizon.net stored as array']
+}
+
+
