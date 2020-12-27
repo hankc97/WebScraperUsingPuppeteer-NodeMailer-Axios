18283b4b89b772fa76f22e12af74a4f2258cdcd5
hankc97 <70183272+hankc97@users.noreply.github.com>
2020-12-26 23:08:51 -0500

    Delete clearInstances.js

--- clearInstances.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    async Failed_Login() {
-        console.log("Failed to login to Discord, Exiting ...");
-        console.log("Browser last on url: " + page.url());
-        page.close();
-        browser.close();
-    },
-}
